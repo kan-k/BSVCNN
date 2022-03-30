0f49d51b81b9e3ca2cfd30c5565f1a1681a7c520
Kan Keeratimahat <qcv214@rescomp1.hpc.in.bmrc.ox.ac.uk>
2022-03-30 16:11:10 +0100

    corrected nns

--- res3/nn_adamWb2.R
+++ res3/nn_adamWb2.R
@@ -1,5 +1,5 @@
 # R script
-#Change for loop for each batch update to matrix (Hadamard) update
+#Adam with regularisation https://arxiv.org/abs/1711.05101
 if (!require("pacman")) {install.packages("pacman");library(pacman)}
 p_load(BayesGPfit)
 p_load(PMS)
@@ -12,17 +12,6 @@ p_load(truncnorm)
 
 JobId=as.numeric(Sys.getenv("SGE_TASK_ID"))
 print("Starting")
-
-if(JobId <= 5){
-  res3.dat <- as.matrix(read_feather('/well/nichols/users/qcv214/bnn2/res3/dat_rearranged.feather'))
-  method <- "re-arranged"
-  print(method)
-}else{
-  res3.dat <- as.matrix(read_feather('/well/nichols/users/qcv214/bnn2/res3/res3_dat.feather'))
-  method <- "raw"
-  print(method)
-}
-
 
 start.time <- Sys.time()
 #1 Split data into mini batches (train and validation)
@@ -50,6 +39,8 @@ mse <- function(pred, true){mean((pred-true)^2)}
 loss.train <- vector(mode = "numeric")
 loss.val <- vector(mode = "numeric")
 
+
+
 print("Loading data")
 
 #Load data and mask and GP 
@@ -57,7 +48,7 @@ print("Loading data")
 res3.mask <-oro.nifti::readNIfTI('/well/nichols/users/qcv214/bnn2/res3/res3mask.nii.gz')
 res3.mask.reg <- sort(setdiff(unique(c(res3.mask)),0))
 #data
-# res3.dat <- as.matrix(read_feather('/well/nichols/users/qcv214/bnn2/res3/dat_rearranged.feather'))
+res3.dat <- as.matrix(read_feather('/well/nichols/users/qcv214/bnn2/res3/res3_dat.feather'))
 #Age
 age_tab<-read_feather('/well/nichols/users/qcv214/bnn2/res3/age.feather')
 age <- age_tab$age
@@ -67,35 +58,50 @@ n.expan <- choose(10+3,3)
 p.dat <- ncol(res3.dat)
 n.dat <- nrow(res3.dat)
 #GP
+
 partial.gp <- array(, dim = c(length(res3.mask.reg),p.dat,n.expan))
 for(i in res3.mask.reg){
   partial.gp[i,,] <- t(as.matrix(read_feather(paste0("/well/nichols/users/qcv214/bnn2/res3/roi/partial_gp_",i,"_fixed_100.540.feather"))))
 }
 partial.gp.centroid<-t(as.matrix(read_feather(paste0("/well/nichols/users/qcv214/bnn2/res3/roi/partial_gp_centroids_fixed_100.540.feather"))))
 
-time.taken <- Sys.time() - start.time
+
+
+time.taken <- Sys.time() - start.time #On RStudio the whole loading up thing takes less than 2 minutes
 cat("Loading data complete in: ", time.taken)
-
-
 print("Getting mini batch")
 #Get minibatch index 
-batch_size <- 500
+batch_size <- 500 #previous 100 batch_size and 50 epochs
 mini.batch <- get_ind_split(num_datpoint = n.dat, num_test = 2000, num_train = 2000,batch_size = batch_size)
 num.batch <- length(mini.batch$train)
-#NN parameters
-learning_rate <-0.7
-epoch <- 10
+
+
+#Hyperparameter
 #Define prior variance of theta
 prior_var <- 0.9
 C2 <- 1/(2*prior_var)
+#NN parameters
+learning_rate <-0.1
+epoch <- 40
 
+#Adam 
+#hyperparameter
+beta1 <- 0.9
+beta2 <- 0.999
+eps <- 1e-8
+#initialisation
+m <- matrix(0,nrow=n.mask, ncol= n.expan )
+v <- matrix(0,nrow=n.mask, ncol= n.expan )
+
+m.b <- rep(0,n.mask)
+v.b <- rep(0,n.mask)
 
 print("Initialisation")
 #1 Initialisation
 #1.1 Initialise the partial weights around normal dist as a matrix of size (nrow(bases..ie choose...) x number of neurons in 2nd layer ie#regions)
 theta.matrix <- matrix(rnorm(n.mask*n.expan,0,prior_var),nrow=n.mask, ncol= n.expan) #Initialise with Norm(0,0.9)
 #1.2 Multiply the partial weights to partial GP and use it as the actual weights of size (p x 1)
-#Initialising weights
+#Initialising
 weights <- matrix(, ncol = p.dat, nrow = n.mask)
 for(i in res3.mask.reg){
   weights[i,] <- partial.gp[i,,] %*% theta.matrix[i,]
@@ -104,6 +110,7 @@ for(i in res3.mask.reg){
 bias <- rep(0,n.mask)
 
 
+num.it <-1
 
 time.train <-  Sys.time()
 
@@ -147,10 +154,6 @@ for(e in 1:epoch){
     hs_pred_SOI <- hs_fit_SOI$post_mean$betacoef[1] + hidden.layer.test %*%beta_fit$HS
     loss.val <- c(loss.val, mse(hs_pred_SOI,age[mini.batch$test]))
     
-    
-    
-    #Update weight
-    
     #4Update the full weights, fit GP against the full weights using HS-prior model to get normally dist thetas
     grad.loss <- age[mini.batch$train[[b]]] - hs_in.pred_SOI
     #grad <- mean.grad.loss* beta_fit$HS * relu.prime() *res3.dat[mini.batch$train[[1]], ]
@@ -158,13 +161,32 @@ for(e in 1:epoch){
     #OR
     #This should be in the same dim as `theta.matrix`, so for updating w_ij, we require beta_fit_j *relu.prime(i)*input(i) then take avaerge over batch
     
-    #Update weight
+    #Update theta
     grad <- array(,dim = c(batch_size,dim(theta.matrix)))
-    for(j in 1:nrow(theta.matrix)){ #nrow of theta.matrix = n.mask
+    for(j in 1:nrow(theta.matrix)){
       grad[,j,] <- -c(grad.loss)*beta_fit$HS[j]*c(relu.prime(hidden.layer[,j]))*res3.dat[mini.batch$train[[b]], ] %*% partial.gp[j,,] 
     }
     #Take batch average
     grad.m <- apply(grad, c(2,3), mean)
+    # Full gradient with regularisation
+    grad.full <- grad.m + C2*theta.matrix
+    
+    #ADAM param updates
+    # print(paste0("mt-1 #NA: ",sum(is.na(c(m)))))
+    # print(paste0("vt-1 #NA: ",sum(is.na(c(v)))))
+    m <- beta1*m + (1-beta1)*grad.full
+    v <- beta2*v + (1-beta2)*grad.full^2
+    
+    # print(paste0("mt #NA: ",sum(is.na(c(m)))))
+    # print(paste0("vt #NA: ",sum(is.na(c(v)))))
+    
+    m.hat <- m/(1-beta1^num.it)
+    v.hat <- v/(1-beta2^num.it)
+    # print(paste0("mhat t #NA: ",sum(is.na(c(m.hat)))))
+    # print(paste0("vhat t #NA: ",sum(is.na(c(v.hat)))))
+    #Update theta matrix
+    theta.matrix <- theta.matrix - learning_rate*m.hat/(sqrt(v.hat)+eps) + C2/batch_size*theta.matrix
+    #Note that updating weights at the end will be missing the last batch of last epoch
     
     #Update bias
     grad.b <- matrix(,nrow = batch_size,ncol = n.mask)
@@ -172,22 +194,24 @@ for(e in 1:epoch){
       grad.b[,j] <- -c(grad.loss)*beta_fit$HS[j]*c(relu.prime(hidden.layer[,j]))
     }
     #Take batch average
-    grad.b.m <- c(apply(grad.b, c(2), mean))
+    grad.b.full <- c(apply(grad, c(2), mean))
     
+    #ADAM param updates
+    m.b <- beta1*m.b + (1-beta1)*grad.b.full
+    v.b <- beta2*v.b + (1-beta2)*grad.b.full^2
+    
+    m.b.hat <- m.b/(1-beta1^num.it)
+    v.b.hat <- v.b/(1-beta2^num.it)
     
     #Update theta matrix
-    theta.matrix <- theta.matrix*(1-learning_rate*C2/batch_size) - learning_rate*grad.m
-    #Note that updating weights at the end will be missing the last batch of last epoch
-    
-    #Update bias
-    bias <- bias - learning_rate*c(grad.b.m)
+    bias <- bias - learning_rate*m.b.hat/(sqrt(v.b.hat)+eps)
     
     #Update weight
     for(i in res3.mask.reg){
       weights[i,] <- partial.gp[i,,] %*% theta.matrix[i,]
     }
     
-    
+    num.it <- num.it+1
     print(paste0("training loss: ",mse(hs_in.pred_SOI,age[mini.batch$train[[b]]])))
     print(paste0("validation loss: ",mse(hs_pred_SOI,age[mini.batch$test])))
   }
@@ -198,8 +222,7 @@ for(e in 1:epoch){
 time.taken <- Sys.time() - time.train
 cat("Training complete in: ", time.taken)
 
-write.csv(rbind(loss.train,loss.val),paste0("/well/nichols/users/qcv214/bnn2/res3/pile/test_",method,"_nnb1_loss_",learning_rate,"_jobid_",JobId,".csv"), row.names = FALSE)
-write_feather(as.data.frame(weights),paste0( '/well/nichols/users/qcv214/bnn2/res3/pile/test_',method,'_nnb1_weights_',learning_rate,"_jobid_",JobId,'.feather'))
-write_feather(as.data.frame(theta.matrix),paste0( '/well/nichols/users/qcv214/bnn2/res3/pile/test_',method,'_nnb1_theta_',learning_rate,"_jobid_",JobId,'.feather'))
-write.csv(bias,paste0( '/well/nichols/users/qcv214/bnn2/res3/pile/test_',method,'_nnb1_bias_',learning_rate,"_jobid_",JobId,".csv"), row.names = FALSE)
-
+write.csv(rbind(loss.train,loss.val),paste0("/well/nichols/users/qcv214/bnn2/res3/pile/nnb_adamW2_loss_",learning_rate,"_jobid_",JobId,".csv"), row.names = FALSE)
+write_feather(as.data.frame(weights),paste0( '/well/nichols/users/qcv214/bnn2/res3/pile/nnb_adamW2_weights_',learning_rate,"_jobid_",JobId,'.feather'))
+write_feather(as.data.frame(theta.matrix),paste0( '/well/nichols/users/qcv214/bnn2/res3/pile/nnb_adamW2_theta_',learning_rate,"_jobid_",JobId,'.feather'))
+write.csv(bias,paste0( '/well/nichols/users/qcv214/bnn2/res3/pile/nnb_adamW2_bias_',learning_rate,"_jobid_",JobId,".csv"), row.names = FALSE)
